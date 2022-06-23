55e8de8fe79726ad8bd08ae2c11c1e676cd9d593
Christopher-Carey-Arrowpos <Chris@arrowpos.com>
2022-06-23 16:12:38 -0500

    .

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App() {
   return (
 
     <Router>
+      <img style={{ position: 'absolute',zIndex:-1,top:0 }} src={require('./assets/Arrow POS banner_BLK.png')} class="card-img-top" alt="..." />
       <div class="container" >
-        {/* <img style={{position:'absolute'}} src={require('./assets/Arrow POS banner_BLK.png')} class="card-img-top" alt="..." /> */}
-        <div style={{flexDirection:'row',display:'flex',justifyContent:'space-evenly',marginBottom:20,marginTop:20}}>
+        <div style={{ flexDirection: 'row', display: 'flex', justifyContent: 'space-evenly', marginBottom: 20, marginTop: 20 }}>
 
           <Link to="/apple">
 
-            <div className="card" style={{ width: "18rem",height:'18rem',alignItems:'center' }}>
+            <div className="card" style={{ width: "18rem", height: '18rem', alignItems: 'center' }}>
               <img style={{ width: 400 }} src={require('./assets/Apple-Logo.png')} class="card-img-top" alt="..." />
               <div class="card-body">
                 <p class="card-text">Apple Developer Account</p>
@@ -32,7 +32,7 @@ function App() {
 
 
           <Link to="/google">
-            <div className="card" style={{ width: "18rem",height:'18rem',alignItems:'center' }}>
+            <div className="card" style={{ width: "18rem", height: '18rem', alignItems: 'center' }}>
               <img style={{ width: 200 }} src={require('./assets/Google__G__Logo.svg.png')} class="card-img-top" alt="..." />
               <div class="card-body">
                 <p class="card-text">Google Developer Account</p>
